2095a32fbb893a0a9753e23b00f17299a3327b66
Alireza Shahbazi (ACBYTES) <shahbaziali40@gmail.com>
2021-09-10 23:02:54 +0430

    is_const, conditional, type_trait changes

--- ReCPP/src/Type_Traits/Type_Traits.h
+++ ReCPP/src/Type_Traits/Type_Traits.h
@@ -2,6 +2,7 @@
 
 namespace ACBYTES
 {
+#pragma region is_same
 	template <typename T, typename T1>
 	struct is_same
 	{
@@ -13,4 +14,23 @@ namespace ACBYTES
 	{
 		static constexpr bool value = true;
 	};
+#pragma endregion is_same
+
+	template <typename T, typename T1, bool Value>
+	struct conditional
+	{
+		//typedef int result;
+	};
+
+	template <typename T>
+	struct is_const
+	{
+		static constexpr bool value = is_same<const T, T>::value;
+	};
+
+	//template <typename T>
+	//struct add_const
+	//{
+	//	typedef  T type;
+	//};
 }

cf8f10a331387f31702ce136d5cb15567b668085
Alireza Shahbazi (ACBYTES) <shahbaziali40@gmail.com>
2021-09-12 05:07:02 +0430

    Implemented Func, Implemented WrapFunctions

--- ReCPP/src/Functional/Function.h
+++ ReCPP/src/Functional/Function.h
@@ -3,97 +3,145 @@
 
 namespace ACBYTES
 {
-	/*
-	A function wrapper that can deal with static, non-static, and member functions.
-	@tparam RT Return type of the function.
-	@tparam Class Class that contains the function.
-	@tparam ArgT Arguments that should be passed to the function.
-	*/
-	template <typename RT, typename Class, typename... ArgT>
 	class Function
 	{
-		typedef RT(Class::* funcType)(ArgT...);
-		Class* _class;
-		funcType _funcPtr;
+		/*
+		A function wrapper that can deal with static, non-static, and member functions.
+		@param RT Return type of the function.
+		@param Class Class that contains the function.
+		@param ArgT Arguments that should be passed to the function.
+		*/
+		template <typename RT, typename Class, typename... ArgT>
+		class Func
+		{
+		public:
+			typedef RT(Class::* funcType)(ArgT...);
+
+		private:
+			Class* _class;
+			funcType _funcPtr;
+
+		public:
+			Func(Class* ClassPtr, funcType FuncPtr)
+			{
+				_class = ClassPtr;
+				_funcPtr = FuncPtr;
+			}
+
+			RT operator()(ArgT... Args)
+			{
+				return (_class->*_funcPtr)(std::forward<ArgT>(Args)...);
+			}
+
+		private:
+			Func() = delete;
+		};
+
+		/*
+		A function wrapper that can deal with static, non-static, and member functions.
+		@param RT Return type of the function.
+		@param Class [Class type that contains the function].
+		*/
+		template <typename RT, typename Class>
+		class Func<RT, Class>
+		{
+		public:
+			typedef RT(Class::* funcType)(void);
+
+		private:
+			Class* _class;
+			funcType _funcPtr;
+
+		public:
+			Func(Class* ClassPtr, funcType FuncPtr)
+			{
+				_class = ClassPtr;
+				_funcPtr = FuncPtr;
+			}
+
+			RT operator()(void)
+			{
+				return (_class->*_funcPtr)();
+			}
+
+		private:
+			Func() = delete;
+		};
+
+		/*
+		A function wrapper that can deal with static, non-static, and member functions.
+		@param RT Return type of the function.
+		@param ArgT Arguments that should be passed to the function.
+		*/
+		template <typename RT, typename... ArgT>
+		class Func<RT, void, ArgT...>
+		{
+		public:
+			typedef RT(*funcType)(ArgT...);
+
+		private:
+			funcType _funcPtr;
+
+		public:
+			Func(funcType FuncPtr)
+			{
+				_funcPtr = FuncPtr;
+			}
+
+			RT operator()(ArgT... Args)
+			{
+				return _funcPtr(std::forward<ArgT>(Args)...);
+			}
+
+		private:
+			Func() = delete;
+		};
+
+		/*
+		A function wrapper that can deal with static, non-static, and member functions.
+		@param RT Return type of the function.
+		*/
+		template <typename RT>
+		class Func<RT, void>
+		{
+		public:
+			typedef RT(*funcType)(void);
+
+		private:
+			funcType _funcPtr;
+
+		public:
+			Func(funcType FuncPtr)
+			{
+				_funcPtr = FuncPtr;
+			}
+
+			RT operator()(void)
+			{
+				return _funcPtr();
+			}
+
+		private:
+			Func() = delete;
+		};
+
 	public:
-
-		Function(Class* ClassPtr, funcType FuncPtr)
+		template<typename RT>
+		static Func<RT, void> WrapFunction(typename Func<RT, void>::funcType FunctionPointer)
 		{
-			_class = ClassPtr;
-			_funcPtr = FuncPtr;
-		}
-	public:
-		RT operator()(ArgT... Args)
-		{
-			return (_class->*_funcPtr)(std::forward<ArgT>(Args)...);
-		}
-	};
-
-	/*
-	A function wrapper that can deal with static, non-static, and member functions.
-	@tparam RT Return type of the function.
-	@tparam Class Class that contains the function.
-	*/
-	template <typename RT, typename Class>
-	class Function<RT, Class, void>
-	{
-		typedef RT(Class::* funcType)(void);
-		Class* _class;
-		funcType _funcPtr;
-	public:
-		Function(Class* ClassPtr, funcType FuncPtr)
-		{
-			_class = ClassPtr;
-			_funcPtr = FuncPtr;
+			return Func<RT, void>(FunctionPointer);
 		}
 
-		RT operator()(void)
+		template<typename RT, typename Class>
+		static Func<RT, Class> WrapFunction(Class* ClassPointer, typename Func<RT, Class>::funcType FunctionPointer)
 		{
-			return (_class->*_funcPtr)();
-		}
-	};
-
-	/*
-	A function wrapper that can deal with static, non-static, and member functions.
-	@tparam RT Return type of the function.
-	@tparam ArgT Arguments that should be passed to the function.
-	*/
-	template <typename RT, typename... ArgT>
-	class Function<RT, void, ArgT...>
-	{
-		typedef RT(*funcType)(ArgT...);
-		funcType _funcPtr;
-	public:
-
-		Function(funcType FuncPtr)
-		{
-			_funcPtr = FuncPtr;
-		}
-	public:
-		RT operator()(ArgT... Args)
-		{
-			return _funcPtr(std::forward<ArgT>(Args)...);
-		}
-	};
-
-	/**
-	A function wrapper that can deal with static, non-static, and member functions.
-	@tparam RT Return type of the function.
-	*/
-	template <typename RT>
-	class Function<RT, void, void>
-	{
-		typedef RT(*funcType)(void);
-		funcType _funcPtr;
-	public:
-		Function(funcType FuncPtr)
-		{
-			_funcPtr = FuncPtr;
+			return Func<RT, Class>(ClassPointer, FunctionPointer);
 		}
 
-		RT operator()(void)
+		template<typename RT, typename Class, typename... ArgT>
+		static Func<RT, Class, ArgT...> WrapFunction(Class* ClassPointer, typename Func<RT, Class>::funcType FunctionPointer)
 		{
-			return _funcPtr();
+			return Func<RT, Class, ArgT...>(ClassPointer, FunctionPointer);
 		}
 	};
 }
--- ReCPP/src/main.cpp
+++ ReCPP/src/main.cpp
@@ -1,28 +1,10 @@
 #include "Type_Traits/Type_Traits.h"
 #include "Functional/Function.h"
 #include <iostream>
-
-class FunctionHolder
-{
-public:
-	void Do() {
-		std::cout << 22;
-	}
-
-	static int F() {
-		std::cout << 88;
-		return 5;
-	}
-
-	static int Add(int A, int B)
-	{
-		std::cout << A + B;
-		return A + B + 1000;
-	}
-};
+#include <typeinfo>
+#include <vector>
 
 int main()
 {
-	ACBYTES::Function<int, void, int, int> a = ACBYTES::Function<int, void, int, int>(FunctionHolder::Add);
-	auto u = a(5, 10);
+
 }
